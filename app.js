b0074bdb1673d787d1293cc795d6f0bb1e7d36c5
ignacy125 <ignacy125@gmail.com>
2017-05-16 17:55:18 +0200

    Poprawka json string, funkcja do obsługi socket emit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(8083);
+app.listen(8084);
 console.log("working");
 
 
@@ -25,22 +25,28 @@ function handler (req, res) {
 
 
 
+function deploy_units_handler (data) {
+    console.log('wybrany kraj: '+ data);
+    fs = require('fs');
+    var users_json = JSON.parse(fs.readFileSync('users.json').toString());
+    user1_units_to_deploy = users_json["users"]["user1"]["units_to_deploy"] -=1;
+    fs.writeFile('users.json', JSON.stringify(users_json));
+    fs1 = require('fs');
+    var dataCountries = JSON.parse(fs1.readFileSync('countries.json').toString());
+
+    var countryUnits = dataCountries["continents"]["North_america"][counutry_name]["units"] +=1;
+    fs1.writeFile('countries.json', JSON.stringify(dataCountries));
+
+
+}
+
+
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
     socket.on('send_deployed_units_amount', function (data) {
-        console.log('przed rozmieszczeniem: '+ data);
-        fs = require('fs');
-        var users_json = JSON.parse(fs.readFileSync('users.json').toString());
-        user1_units_to_deploy = users_json["users"]["user1"]["units_to_deploy"] -=1;
-        fs.writeFile('users.json', JSON.stringify(users_json));
-        fs1 = require('fs');
-        var dataCountries = JSON.parse(fs1.readFileSync('countries.json').toString());
-        var alaska_units = dataCountries["continents"]["North_america"]["Alaska"]["units"] +=1;
-        fs1.writeFile('countries.json', JSON.stringify(dataCountries));
+        deploy_units_handler(data);
 
 
-        console.log('po rozmieszczeniu Alaska units '+alaska_units);
-        console.log('po rozmieszczeniu user 1 units to deploy '+user1_units_to_deploy);
     });
   socket.on('testowy_event', function (data) {
     console.log(data);
